1c8a227b3412bbee23d4d4a9611b5387243a937f
proedison <50390910+proedison@users.noreply.github.com>
2019-05-28 12:47:01 +0800

    Add files via upload

---
+++ wechat_open - clc.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set wechatpath=D:\WeChat\WeChat.exe
+echo.
+
+set /p a=请输入微信多开数目，请输入数字后然后回车启动: 
+
+for /l %%i in (1,1,%a%) do start /b %wechatpath%
+
+>nul ping 127.1 -n 3
+
+cls
+
+echo.
+
+pause
---
+++ wechat_open.bat
@@ -0,0 +1,8 @@
+TASKKILL /F /IM wechat.exe
+
+start "" "D:\WeChat\WeChat.exe"
+
+start "" "D:\WeChat\WeChat.exe"
+
+
+
